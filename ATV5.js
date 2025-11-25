ccec227334aee52995613d9983ef5f0d0279088f
carlos <carlos-2025170@evl.com.br>
2025-11-25 20:26:40 -0300

    Nova versao

--- ATV5.js
+++ ATV5.js
@@ -1,25 +1,41 @@
 /*
 for(i=1; i<=10; i++){
-    //if(i % 2 === 0)
+    if(i % 2 === 0)
     console.log(i);
 }
 */
-/*-
+/*
 for(i=10; i>=1; i--){
     //if(i % 2 === 0)
     console.log(i);
 }
 */
+/*
 let par = 0;
 let impar = 0;
 for(i=0; i<10; i++){
    const numero = Number(prompt("Diga um numero:"));
-   console.log(numero); 
+   //console.log(numero); 
    if(numero % 2 === 0){
         par = par +1
-        console.log("Par");
+        //console.log("Par");
     }else{
         impar = impar +1
-        console.log("Impar");
+       // console.log("Impar");
+    }
+}
+console.log(`Os numeros par sao:${par}`);
+console.log(`Os numeros impar sao:${impar}`);
+*/
+const numeroEscolhido = 7;
+for(i=0; i<=7; i++){
+    const numero = Number(prompt("Digar um numero"));
+    if(numero < numeroEscolhido){
+        console.log("È um numero maior");
+    }else if(numero > numeroEscolhido){
+        console.log("È um numero menor");
+    }else{
+        console.log("Parabens, voce acertou");
+        break;
     }
 }
