66addbac3c4fb2b5e0e06cda2a5a1fe5ac18db7c
carlos <carlos-2025170@evl.com.br>
2025-11-26 21:18:06 -0300

    Atualizaçoes da atividade

--- ATV5.js
+++ ATV5.js
@@ -42,10 +42,46 @@ for(i=0; i<=7; i++){
 }
 */
 /*Atividade For*/
-/*
+/*1
 const numero = Number(prompt("Diga um numero:"));
 for(i=numero; i>=1; i--){
     console.log(i);
 }
 */
+/*2
+let par = 0;
+const numero = Number(prompt("Diga o numero escolhido"));
+for(i=2; i <= numero; i++){
+    if(i % 2 === 0){
+        par = par + i;
+    }
+}
+console.log(`O seu numero ${numero} é ${par}`);
+*/
+/*3*/
+/*
+const numero = (prompt("Digar o seu numero"));
+console.log(`Tabuada de ${numero}:`);
+for(let i = 1; i <= 10; i++){
+console.log(`${numero} x ${i} = ${numero * i}`);
+}
+*/
+/*4*/
+/*
+let quadrado = 0;
+const numero = Number(prompt("Diga o numero escolhido"));
+for(i=1; i <= numero; i++){
+        quadrado = quadrado + (i ** 2);
+}
+console.log(`O seu numero ${numero} é ${quadrado}`);
+*/
 
+/*Lista 2*/
+/*
+const numero = Number(prompt("Insirar um numero:"));
+let soma = 0;
+for(let i=1; i<=numero; i++){
+    soma = soma + (i + i);
+    console.log(`${soma}`);
+}
+*/
