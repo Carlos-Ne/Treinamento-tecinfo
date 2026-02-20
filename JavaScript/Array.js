313c57e039a2e7095871e3e29fc3508482ca9e23
carlos <carlos-2025170@evl.com.br>
2026-02-20 20:20:05 -0300

    Indroduzindo o Array.

---
+++ JavaScript/Array.js
@@ -0,0 +1,11 @@
+//Lista de execircio UM:
+ 
+const temperaturaMedia = [];
+ temperaturaMedia[0] = 31.9;
+ temperaturaMedia[1] = 35.4;
+ temperaturaMedia[2] = 25.4;
+ temperaturaMedia[3] = 22.9;
+
+ for(i = 0; i < temperaturaMedia.length; i++){
+     console.log(temperaturaMedia[i]);
+ }
